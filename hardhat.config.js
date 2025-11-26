52b90872ca4f0f700535675088e20fac3fcd3da1
mansijoshi17 <mansijoshi1777@gmail.com>
2025-11-26 17:52:00 +0530

    Added configuration

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: "0.8.20",
+    networks: {
+        hardhat: {},
+        // Add your testnet here later (e.g. sepolia, polygon, etc.)
+    }
+};
